9c34f591a03a370d56ad45ce6d020dfa9c82cc71
Jamshidbek <jamshidbekmukhammedov@gmail.com>
2025-12-25 02:08:27 +0900

    BRR: build express web server

---
+++ server.js
@@ -0,0 +1,31 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const { read } = require("fs");
+const app = express();
+const http = require("http");
+
+// 1: Kirish code
+app.use(express.static("public"));
+app.use(express.json());                // kirib kelayotgan json formatdagi data ni object holatiga o'girib beradi!
+app.use(express.urlencoded({extended: true}));
+
+// 2: Session code
+
+// 3: Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing code
+app.get("/", function(req, res) {
+    res.end(`<h1 style="background: red">HELLO WORLD by Marco</h1>`);
+});
+
+app.get("/gift", function(req, res) {
+    res.end(`<h1 style="background: red">Siz sovg'alar bo'limidasiz</h1>`);
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function () {
+    console.log(`The server is running succsessfully on port: ${PORT}`);
+});
