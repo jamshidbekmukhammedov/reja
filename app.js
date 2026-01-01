b24a8c25f7c1ecb66ffa8dff1ff4358a69434d66
Jamshidbek <jamshidbekmukhammedov@gmail.com>
2026-01-01 10:14:53 +0900

    feat: CRUD operatsiyalari

--- app.js
+++ app.js
@@ -31,20 +31,34 @@ app.set("view engine", "ejs");
 
 // 4 Routing code
 
-// app.get("/hello", function(req, res) {
-//     res.end(`<h1 style="background: red">HELLO WORLD by Marco</h1>`);
-// });
+// app.get("/author", (req, res) => {
+//     res.render("author", {user: user});
+// })
 
-// app.get("/gift", function(req, res) {
-//     res.end(`<h1 style="background: red">Siz sovg'alar bo'limidasiz</h1>`);
-// });
-
-app.get("/author", (req, res) => {
-    res.render("author", {user: user});
-})
+app.post("/create-item", (req, res) => {
+    console.log('user entered /create-item');
+    console.log(req.body);
+    const new_reja = req.body.reja;
+    db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+        if(err) {
+            console.log(err);
+            res,end('something went wrong');
+        } else {
+            res.end('successfully edded');
+        }
+    });
+});
 
 app.get("/", function (req, res) {
-    res.render("reja");
+    console.log('user entered /');
+    db.collection("plans").find().toArray((err, data) => {
+        if(err) {
+            console.log;pg(err);
+            res.end("something went wrong");
+        } else {
+            res.render("reja", {items: data});
+        }
+    });
 })
 
 module.exports = app;
