37e90e2e87dbb2a12801c91401519ba1a212953e
Jamshidbek <jamshidbekmukhammedov@gmail.com>
2026-01-01 10:37:09 +0900

    feat: train.js file updated

--- train.js
+++ train.js
@@ -1,5 +1,26 @@
 console.log("TRAIN AREA!");
 
+// TASK B: 
+// Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+// MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+
+
+function countDigits(findNumber) {
+    let count = 0;
+    for (let i = 0; i < findNumber.length; i++) {
+        if (findNumber[i] >= '0' && findNumber[i] <= '9') {
+            count++;
+        }
+    }
+    return count;
+}
+const result = countDigits("ad2a54y79wet0sfgb9");
+console.log("result:", result);
+
+
+// ========================================================
+
+
 // A-TASK:
 // Shunday 2 parametrli function tuzingki 
 // birinchi parametrdagi letterni ikkinchi 
@@ -8,14 +29,14 @@ console.log("TRAIN AREA!");
 
 // Masala Yechimi:
 
-function countLetter(a, b) {
-  let count = 0;
-  for (let i = 0; i < b.length; i++) {
-    if (b[i] === a) {
-      count++;
-    }
-  }
-  return count;
-}
+// function countLetter(a, b) {
+//   let count = 0;
+//   for (let i = 0; i < b.length; i++) {
+//     if (b[i] === a) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
 
-console.log("Natija:", countLetter("e", "engineer"));
+// console.log("Natija:", countLetter("e", "engineer"));
